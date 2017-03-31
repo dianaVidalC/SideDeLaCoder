e7772dcc1d7fc8774d7a1d5d4851b61acd8ad4d1
Erika Vidal <diann852@gmail.com>
2017-03-31 13:45:41 -0500

    primer commit

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+var boton=document.getElementById("boton");
+
+boton.addEventListener("click",function(e){
+  e.preventDefault();
+  
+  var postear=document.getElementById("text");
+  var resultado=document.getElementById("post");
+  var post = document.createElement('div');
+     var texto = document.createElement('p');
+     texto.innerHTML = texto;
+     post.classList.add("posteo");
+     post.appendChild(text);
+     resultado.appendChild();
+});
