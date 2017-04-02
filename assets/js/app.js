4ce7b789f8d94fd8282e1ef9d9c4efe6d1b33f12
Erika Vidal <erikadianavidalcapillo@gmail.com>
2017-04-01 19:41:54 -0500

    publicando post

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,45 @@
-var boton=document.getElementById("boton");
-
-boton.addEventListener("click",function(e){
-  e.preventDefault();
-  
-  var postear=document.getElementById("text");
+window.addEventListener("load",function(){
+  var boton=document.getElementById("boton");
+  var postear=document.getElementById("texto");
+  var coder=document.getElementById("coder");
   var resultado=document.getElementById("post");
-  var post = document.createElement('div');
-     var texto = document.createElement('p');
-     texto.innerHTML = texto;
-     post.classList.add("posteo");
-     post.appendChild(text);
-     resultado.appendChild();
+  var usuaria=document.getElementById("coder");
+  var errorUsuaria=document.getElementById("error-usuaria");
+  var errorTexto=document.getElementById("error-texto");
+
+  boton.addEventListener("click",function(e){
+    e.preventDefault();
+
+      if (usuaria.value!="" && postear.value!="") {
+        var post = document.createElement('div');
+        var texto = document.createElement('p');
+        var autor=document.createElement("p");
+           post.classList.add("posteo");
+           autor.classList.add("autor");
+           texto.classList.add("contenido");
+           texto.appendChild(document.createTextNode(postear.value));
+           autor.appendChild(document.createTextNode(coder.value));
+           post.appendChild(texto);
+           post.appendChild(autor);
+           resultado.insertBefore(post,resultado.childNodes[0]);
+           document.getElementById("sign-in").reset();
+      }else{
+        if (usuaria.value=="" && postear.value=="") {
+           errorUsuaria.appendChild(document.createTextNode("*Ingrese usuaria"));
+           errorTexto.appendChild(document.createTextNode("*Ingrese texto"));
+           coder.focus();
+           return false;
+        }else if (usuaria.value!="" && postear.value=="") {
+          errorTexto.appendChild(document.createTextNode("*Ingrese texto"));
+          errorUsuaria.innerHTML="";
+          coder.focus();
+          postear.focus;
+          return false;
+        }else if (usuaria.value=="" && postear.value!="") {
+          errorUsuaria.appendChild(document.createTextNode("*Ingrese usuaria"));
+          errorTexto.innerHTML="";
+          coder.focus();
+        }
+      }
+   });
 });
